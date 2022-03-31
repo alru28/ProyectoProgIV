9f474c26b2bcce8f342073d84d9957bed37f7f50
Alvaro Ruiz <alvaroruiz@opendeusto.es>
2022-03-31 18:43:06 +0200

    Funcion cargaConfig
    Funcion que carga toda la informacion que haya en el fichero config.txt y la devuelve en un array de strings (Más doc en el .h)

---
+++ Config/config.c
@@ -0,0 +1,30 @@
+#include "config.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char **cargarConfig(char* rutaConfig) {
+	FILE *config = fopen(rutaConfig, "r");
+	char caracter = '0';
+	int contString = 0;
+
+	char **strings = (char **) malloc(5 * sizeof(char *));
+
+	while (caracter != EOF) {
+		caracter = fgetc(config); // Mientras no se llegue al final del archivo de config, se sigue
+		printf("\nCaracter recibido: %c", caracter);
+		if (caracter == '=') {	  // Si detecta el caracter =, se pilla toda la string que haya después de él
+			char* stringDinamica = (char*)malloc(30 * sizeof(char) + 1);
+			stringDinamica = fgets(stringDinamica, 30, config);								
+			printf("\nHe pillado stringDinamica: %s", stringDinamica);
+			stringDinamica[strlen(stringDinamica) - 1] = '\0';		// Quito el caracter \n del final sustituyendo por \0
+			strings[contString] = stringDinamica;
+			printf("\nHe guardado stringDinamica: %s", strings[contString]); // La añado al array de strings que devuelve la func
+			contString++;
+
+		}
+	}
+	fclose(config);
+
+	return strings;
+}
---
+++ Config/config.h
@@ -0,0 +1,15 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+/** 
+* Funcion que carga un fichero de texto de configuracion y devuelve un puntero a todas las strings que haya despues del
+* caracter =
+* @param rutaConfig es la ruta del fichero de configuracion
+* @return devuelve un array de strings con todas las posibles strings con valor para la configuracion
+* @see testConfig.c
+*/
+char **cargarConfig(char* rutaConfig);
+
+// void escribirConfig(char* rutaConfig);  OPCIONAL
+
+#endif
