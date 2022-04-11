0cac61a2e77e2ceb66f36202bf595d15f118728d
jorgealcorta <jorgealcorta@opendeusto.es>
2022-04-11 23:43:47 +0200

    Resuelto conflicto mostrarLotesActivos y idUsing

--- BaseDatos/GestorBaseDatos.c
+++ BaseDatos/GestorBaseDatos.c
@@ -6,6 +6,48 @@
 #include "sqlite3.h"
 #include "GestorBaseDatos.h"
 
+int idUsing = -1;
+
+int login(sqlite3 *db, char* username, char* password ){
+
+    sqlite3_stmt *stmt;
+    char sql[100];
+    sprintf(sql, "select Contraseña, ID_Usuario from Usuario where %i = Nombre", username);
+    
+    int result = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL) ;
+    
+	if (result != SQLITE_OK) {
+		printf("Error preparing statement (SELECT)\n");
+		printf("%s\n", sqlite3_errmsg(db));
+		return 0;
+	}
+
+    result = sqlite3_step(stmt) ;
+
+    char Contrasenya [20];
+    int idUser;
+
+    if(result == SQLITE_ROW){
+        
+		strcpy(Contrasenya, (char *) sqlite3_column_text(stmt, 0));
+        idUser= sqlite3_column_int(stmt, 1);
+        
+    }else{
+        printf("Error, Username not found");
+        return 0;
+    }
+
+    if(Contrasenya == password){
+        idUsing = idUser;
+        return 1;
+    }else {
+        printf("contrasenyas no coinciden");
+        return 0;
+    }
+}
+
+
+
 
 sqlite3* cargarBaseDatos(char* rutaBaseDatos) {
     sqlite3* db;
@@ -204,9 +246,61 @@ int mostrarDia(sqlite3 *db , char *dia){
       
 }
 
+int mostrarLotesActivos(sqlite3 *db){
+    
+    sqlite3_stmt *stmt;
+    
+    char sql[100];
+    sprintf(sql, "select ID_Lote, FechaCom, FechaFin, AvgPrecio from lote where Estado = 'ACTIVO'");
+
+    printf("%s", sql);
+
+    int result = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL) ;
+	if (result != SQLITE_OK) {
+		printf("Error preparing statement (SELECT)\n");
+		printf("%s\n", sqlite3_errmsg(db));
+		return 0;
+	}
+    printf("\nLotes activos actualmente :\n");
+
+	do {
+		result = sqlite3_step(stmt) ;
+		if (result == SQLITE_ROW) {
+			int id= sqlite3_column_int(stmt, 0);
+            char fechaInicio[20];
+			strcpy(fechaInicio, (char *) sqlite3_column_text(stmt, 1));
+            char fechaFinal[20];
+			strcpy(fechaFinal, (char *) sqlite3_column_text(stmt, 2));
+            float avgPrecio = (float) sqlite3_column_double(stmt, 4);
+            printf("\n");
+
+            if(avgPrecio<10.00){
+                printf("$");
+            } else if(avgPrecio <30.00){
+                printf("$$");
+            }else if(avgPrecio < 60.00){
+                printf("$$$");
+            }else{
+                printf("$$$$");
+            }
+            printf("\n");            
+		}
+	} while (result == SQLITE_ROW);
+
+    result = sqlite3_finalize(stmt);
+	if (result != SQLITE_OK) {
+		printf("Error finalizing statement (SELECT)\n");
+		printf("%s\n", sqlite3_errmsg(db));
+		return 0;
+	}
+    
+    return 1;
+    
+}
+
 // Introduce un objeto pasado como argumento a la base de datos db
-int introducirObjeto(Objeto* objeto){
-    sqlite3 *db = cargarBaseDatos("basedatos.db");
+int introducirObjeto(sqlite3 *db, Objeto* objeto){
+    
     sqlite3_stmt *stmt;
     char sql[200];
     sprintf(sql, "INSERT INTO objeto ( Estado, Categoria, Descripcion, PrecioSalida, ID_Subastador, ID_Lote) VALUES ('%s', '%s', '%s', %.2f, %i, %i);", objeto->Estado, objeto->Categoria, objeto->Descripcion, objeto->PrecioSalida, objeto->ID_Subastador, objeto->ID_Lote);
