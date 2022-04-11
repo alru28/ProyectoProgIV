57fd13458583a468b6eda63db99cf4b34db74d0f
Unai Igartua <unaiigartua@opendeusto.es>
2022-04-11 23:15:10 +0200

    al final no hacemos la función existeUsuario

--- BaseDatos/duplicado.h
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "../Objeto/Objeto.h"
-#include "../Lote/Lote.h"
-#include "sqlite3.h"
-#include "GestorBaseDatos.h"
-
-int existeUsuario(sqlite3 *db, char *nombreUsuario);
