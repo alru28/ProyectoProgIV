9553652721de9d0fe71d629552ea8f61b70343af
nicllop <nicllop@opendeusto.es>
2022-04-12 10:46:05 +0200

    pruebas para que funcione el login desde el main

--- Main/Main.c
+++ Main/Main.c
@@ -3,10 +3,14 @@
 #include "../BaseDatos/GestorBaseDatos.h"
 #include <stdio.h>
 
+//gcc ../Main/Main.c ../Objeto/Objeto.c GestorBaseDatos.c ../Lote/Lote.c sqlite3.c   ../Menu/Menu.c -o main.exe
+
 int main(void)
 {
     sqlite3 *db = cargarBaseDatos("basedatos.db");
     sqlite3_stmt *stmt;
+    login(db, "jaime_col", "palencia998");
+    printf("%i", idUsing);
 
     
     
