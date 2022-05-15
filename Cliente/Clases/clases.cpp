91e9c458a56b463cd01ebac5f4323c84eb8d93f2
Alvaro Ruiz <alvaroruiz@opendeusto.es>
2022-05-15 12:05:38 +0200

    Inicializacion basica de clases del cliente en c++
    Faltan por hacer:
    - salidas por pantalla (sobrecargando << con funcion friend / cout clasico con metodo imprimir)
    - metodo eliminarObjeto

---
+++ Cliente/Clases/clases.cpp
@@ -0,0 +1,417 @@
+#include "clases.h"
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+namespace clases {
+
+
+	Cartera::Cartera(int idCartera, float saldo, int idUsuario) {
+		this->ID_Cartera = idCartera;
+		this->ID_Usuario = idUsuario;
+		this->saldo = saldo;
+	}
+	Cartera::Cartera(const Cartera& c) {
+		this->ID_Cartera = c.ID_Cartera;
+		this->ID_Usuario = c.ID_Usuario;
+		this->saldo = c.saldo;
+	}
+
+	Transaccion::Transaccion(int idTrans, float cantidad, int fecha, int idObjeto, int idCartEnvia, int idCartRec) {
+		this->ID_Transaccion = idTrans;
+		this->Cantidad = cantidad;
+		this->Fecha = fecha;
+		this->ID_Objeto = idObjeto;
+		this->ID_Cartera_Envia = idCartEnvia;
+		this->ID_Cartera_Recibe = idCartRec;
+	}
+	Transaccion::Transaccion(const Transaccion& t) {
+		this->ID_Transaccion = t.ID_Transaccion;
+		this->Cantidad = t.Cantidad;
+		this->Fecha = t.Fecha;
+		this->ID_Objeto = t.ID_Objeto;
+		this->ID_Cartera_Envia = t.ID_Cartera_Envia;
+		this->ID_Cartera_Recibe = t.ID_Cartera_Recibe;
+	}
+
+	// Getters y Setters
+	int Transaccion::getIDTrans() {
+		return this->ID_Transaccion;
+	}
+	float Transaccion::getCant() {
+		return this->Cantidad;
+	}
+	int Transaccion::getFecha() {
+		return this->Fecha;
+	}
+	int Transaccion::getIDObjeto() {
+		return this->ID_Objeto;
+	}
+	int Transaccion::getIDCarteraRecibe() {
+		return this->ID_Cartera_Recibe;
+	}
+	int Transaccion::getIDCarteraEnvia() {
+		return this->ID_Cartera_Envia;
+	}
+
+	void Transaccion::setIDTrans(int id) {
+		this->ID_Transaccion = id;
+	}
+	void Transaccion::setCant(int cant) {
+		this->Cantidad = cant;
+	}
+	void Transaccion::setFecha(int fecha) {
+		this->Fecha = fecha;
+	}
+	void Transaccion::setIDObjeto(int idObjeto) {
+		this->ID_Objeto = idObjeto;
+	}
+	void Transaccion::setIDCarteraRecibe(int idCartera) {
+		this->ID_Cartera_Recibe = idCartera;
+	}
+	void Transaccion::setIDCarteraEnvia(int idCartera) {
+		this->getIDCarteraEnvia = idCartera;
+	}
+
+	// Constructores y destructores
+	Objeto::Objeto(int idObjeto, char* cat, char* estado, char* desc, float precioSal, int idSubastador, int idLote) {
+		this->ID_Objeto = idObjeto;
+		this->Categoria = new char[strlen(cat) + 1];
+		strcpy(this->Categoria, cat);
+		this->Estado = new char[strlen(estado) + 1];
+		strcpy(this->Estado, estado);
+		this->Descripcion = new char[strlen(desc) + 1];
+		strcpy(this->Descripcion, desc);
+		this->PrecioSalida = precioSal;
+		this->ID_Subastador = idSubastador;
+		this->ID_Lote = idLote;
+	}
+	Objeto::Objeto(const Objeto& o) {
+		this->ID_Objeto = o.ID_Objeto;
+		this->Categoria = new char[strlen(o.Categoria) + 1];
+		strcpy(this->Categoria, o.Categoria);
+		this->Estado = new char[strlen(o.Estado) + 1];
+		strcpy(this->Estado, o.Estado);
+		this->Descripcion = new char[strlen(o.Descripcion) + 1];
+		strcpy(this->Descripcion, o.Descripcion);
+		this->PrecioSalida = o.PrecioSalida;
+		this->ID_Subastador = o.ID_Subastador;
+		this->ID_Lote = o.ID_Lote;
+	}
+	Objeto::~Objeto() {
+		delete[] this->Categoria;
+		delete[] this->Estado;
+		delete[] this->Descripcion;
+	}
+	// Getters y Setters
+
+	int Objeto::getIDObjeto() {
+		return this->ID_Objeto;
+	}
+	char* Objeto::getCategoria() {
+		return this->Categoria;
+	}
+	char* Objeto::getEstado() {
+		return this->Estado;
+	}
+	char* Objeto::getDescripcion() {
+		return this->Descripcion;
+	}
+	float Objeto::getPrecioSalida() {
+		return this->PrecioSalida;
+	}
+	int Objeto::getIDSubastador() {
+		return this->ID_Subastador;
+	}
+	int Objeto::getID_Lote() {
+		return this->ID_Lote;
+	}
+
+	void Objeto::setIDObjeto(int id) {
+		this->ID_Objeto = id;
+	}
+	void Objeto::setCategoria(char* categoria) {
+		delete[] this->Categoria;
+		this->Categoria = new char[strlen(categoria) + 1];
+		strcpy(this->Categoria, categoria);
+
+	}
+	void Objeto::setEstado(char* estado) {
+		delete[] this->Estado;
+		this->Estado = new char[strlen(estado) + 1];
+		strcpy(this->Estado, estado);
+	}
+	void Objeto::setDescripcion(char* descripcion) {
+		delete[] this->Descripcion;
+		this->Descripcion = new char[strlen(descripcion) + 1];
+		strcpy(this->Descripcion, descripcion);
+	}
+	void Objeto::setPrecioSalida(float precio) {
+		this->PrecioSalida = precio;
+	}
+	void Objeto::setIDSubastador(int id) {
+		this->ID_Subastador = id;
+	}
+	void Objeto::setIDLote(int id) {
+		this->ID_Lote = id;
+	}
+
+	// Metodos
+	void Objeto::imprimirObjeto() {
+		// TDB
+	}
+
+	// Constructores y destructores
+	Lote::Lote(int idLote, int fechaCom, int fechaFin, char* estado, int avgPrecio, int cantObj, Objeto* listaObjetos) {
+		this->idLote = idLote;
+		this->fechaCom = fechaCom;
+		this->fechaFin = fechaFin;
+		this->estado = new char[strlen(estado) + 1];
+		strcpy(this->estado, estado);
+		this->avgPrecio = avgPrecio;
+		this->cantidadObjetos = cantidadObj;
+		this->listaObjetos = new Objeto[cantObj];
+		for (int i = 0; i < cantObj; i++) {
+			this->listaObjetos[i] = listaObjetos[i];
+		}
+	}
+	Lote::Lote(const Lote& l) {
+		this->idLote = l.idLote;
+		this->fechaCom = l.fechaCom;
+		this->fechaFin = l.fechaFin;
+		this->estado = new char[strlen(l.estado) + 1];
+		strcpy(this->estado, l.estado);
+		this->avgPrecio = l.avgPrecio;
+		this->cantidadObjetos = l.cantidadObjetos;
+		this->listaObjetos = new Objeto[l.cantidadObjetos];
+		for (int i = 0; i < l.cantidadObjetos; i++) {
+			this->listaObjetos[i] = l.listaObjetos[i];
+		}
+	}
+	Lote::~Lote() {
+		delete[] this->listaObjetos;
+		delete[] this->estado;
+	}
+	// Getters y Setters
+
+	int Lote::getIDLote() {
+		return this->idLote;
+	}
+	int Lote::getFechaCom() {
+		return this->fechaCom;
+	}
+	int Lote::getFechaFin() {
+		return this->fechaFin;
+	}
+	char* Lote::getEstado() {
+		return this->estado;
+	}
+	int Lote::getAvgPrecio() {
+		return this->avgPrecio;
+	}
+	int Lote::getCantidadObjetos() {
+		return this->cantidadObjetos;
+	}
+	Objeto* Lote::getListaObjetos() {
+		return this->listaObjetos;
+	}
+
+	void Lote::setIDLote(int id) {
+		this->idLote = id;
+	}
+	void Lote::setFechaCom(int fecha) {
+		this->fechaCom = fecha;
+	}
+	void Lote::setFechaFin(int fecha) {
+		this->fechaFin = fecha;
+	}
+	void Lote::setEstado(char* estado) {
+		delete[] this->estado;
+		this->estado = new char[strlen(estado) + 1];
+		strcpy(this->estado, estado);
+	}
+	// setAvgPrecio no tiene sentido, se tiene que calcular en el constructor y al añadir/eliminar objetos
+	void Lote::setCantidadObjetos(int cant) {
+		this->cantidadObjetos = cant;
+	}
+	void Lote::setListaObjetos(Objeto* listaObjetos, int cantidadObjetos) {
+		delete[] this->listaObjetos;
+		this->listaObjetos = new Objeto[cantidadObjetos];
+		for (int i = 0; i < cantidadObjetos; i++) {
+			this->listaObjetos[i] = listaObjetos[i];
+		}
+	}
+
+	// Metodos
+	void Lote::anadirObjeto(Objeto& o) {
+		Objeto* aux[this->cantidadObjetos];
+		for (int i = 0; i < this->cantidadObjetos; i++) {
+			aux[i] = this->listaObjetos[i];
+		}
+
+		this->cantidadObjetos++;
+
+		delete[] this->listaObjetos;
+		this->listaObjetos = new Objeto[this->cantidadObjetos];
+		for (int i = 0; i < this->cantidadObjetos - 1; i++) {
+			this->listaObjetos[i] = aux[i];
+		}
+		this->listaObjetos[this->cantidadObjetos - 1] = o;
+		this->calcularAvgPrecio();
+	}
+	void Lote::calcularAvgPrecio() {
+		int sumaTotal = 0;
+		for (int i = 0; i < this->cantidadObjetos; i++) {
+			sumaTotal += this->listaObjetos[i].getPrecioSalida();
+		}
+		this->avgPrecio = sumaTotal / this->cantidadObjetos;
+	}
+	void Lote::eliminarObjeto(int idObjeto) {
+		// Tengo sueño ns como hacerlo ahora mismo :C ZzZzZ
+	}
+	void Lote::imprimirLote() {
+		// TBD
+	}
+
+
+	// Constructores y destructores
+	Usuario::Usuario(int idUsuario, char* contra, char* nombre, int tlf, char* mail, int puntos, int idCartera, char* pais, char* ciudad, char* calle, char* pisoPuerta) {
+		this->ID_Usuario = idUsuario;
+		this->Contrasenia = new char[strlen(contra) + 1];
+		strcpy(this->Contrasenia, contra);
+		this->Nombre = new char[strlen(nombre) + 1];
+		strcpy(this->Nombre, nombre);
+		this->Tlf = tlf;
+		this->Mail = new char[strlen(mail) + 1];
+		strcpy(this->Mail, mail);
+		this->Puntos = puntos;
+		this->ID_Cartera = idCartera;
+		this->Pais = new char[strlen(pais) + 1];
+		strcpy(this->Pais, pais);
+		this->Ciudad = new char[strlen(ciudad) + 1];
+		strcpy(this->Ciudad, ciudad);
+		this->Calle = new char[strlen(calle) + 1];
+		strcpy(this->Calle, calle);
+		this->PisoPuerta = new char[strlen(pisoPuerta) + 1];
+		strcpy(this->PisoPuerta, pisoPuerta);
+
+	}
+	Usuario::Usuario(const Usuario& u) {
+		this->ID_Usuario = u.ID_Usuario;
+		this->Contrasenia = new char[strlen(u.Contrasenia) + 1];
+		strcpy(this->Contrasenia, u.Contrasenia);
+		this->Nombre = new char[strlen(u.Nombre) + 1];
+		strcpy(this->Nombre, u.Nombre);
+		this->Tlf = u.Tlf;
+		this->Mail = new char[strlen(u.Mail) + 1];
+		strcpy(this->Mail, u.Mail);
+		this->Puntos = u.Puntos;
+		this->ID_Cartera = u.ID_Cartera;
+		this->Pais = new char[strlen(u.Pais) + 1];
+		strcpy(this->Pais, u.Pais);
+		this->Ciudad = new char[strlen(u.Ciudad) + 1];
+		strcpy(this->Ciudad, u.Ciudad);
+		this->Calle = new char[strlen(u.Calle) + 1];
+		strcpy(this->Calle, u.Calle);
+		this->PisoPuerta = new char[strlen(u.PisoPuerta) + 1];
+		strcpy(this->PisoPuerta, u.PisoPuerta);
+	}
+	Usuario::~Usuario() {
+		delete[] this->Contrasenia;
+		delete[] this->Nombre;
+		delete[] this->Mail;
+		delete[] this->Pais;
+		delete[] this->Ciudad;
+		delete[] this->Calle;
+		delete[] this->PisoPuerta;
+	}
+	// Getters y Setters
+
+	int Usuario::getIDUsuario() {
+		return this->ID_Usuario;
+	}
+	char* Usuario::getContrasenia() {
+		return this->Contrasenia;
+	}
+	char* Usuario::getNombre() {
+		return this->Nombre;
+	}
+	int Usuario::getTlf() {
+		return this->Tlf;
+	}
+	char* Usuario::getMail() {
+		return this->Mail;
+	}
+	int Usuario::getPuntos() {
+		return this->Puntos;
+	}
+	int Usuario::getIDCartera() {
+		return this->ID_Cartera;
+	}
+	char* Usuario::getPais() {
+		return this->Pais;
+	}
+	char* Usuario::getCiudad() {
+		return this->Ciudad;
+	}
+	char* Usuario::getCalle() {
+		return this->Calle;
+	}
+	char* Usuario::getPisoPuerta() {
+		return this->PisoPuerta;
+	}
+
+	void Usuario::setIDUsuario(int id) {
+		this->ID_Usuario = id;
+	}
+	void Usuario::setContrasenia(char* contrasenia) {
+		delete[] this->Contrasenia;
+		this->Contrasenia = new char[strlen(contrasenia) + 1];
+		strcpy(this->Contrasenia, contrasenia);
+	}
+	void Usuario::setNombre(char* nombre) {
+		delete[] this->Nombre;
+		this->Nombre = new char[strlen(nombre) + 1];
+		strcpy(this->Nombre, nombre);
+	}
+	void Usuario::setTlf(int tlf) {
+		this->Tlf = tlf;
+	}
+	void Usuario::setMail(char* mail) {
+		delete[] this->Mail;
+		this->Mail = new char[strlen(mail) + 1];
+		strcpy(this->Mail, mail);
+	}
+	void Usuario::setPuntos(int puntos) {
+		this->Puntos = puntos;
+	}
+	void Usuario::setIDCartera(int id) {
+		this->ID_Cartera = id;
+	}
+	void Usuario::setPais(char* pais) {
+		delete[] this->Pais;
+		this->Pais = new char[strlen(pais) + 1];
+		strcpy(this->Pais, pais);
+	}
+	void Usuario::setCiudad(char* ciudad) {
+		delete[] this->Ciudad;
+		this->Ciudad = new char[strlen(ciudad) + 1];
+		strcpy(this->Ciudad, ciudad);
+	}
+	void Usuario::setCalle(char* calle) {
+		delete[] this->Calle;
+		this->Calle = new char[strlen(calle) + 1];
+		strcpy(this->Calle, calle);
+	}
+	void Usuario::setPisoPuerta(char* pisoPuerta) {
+		delete[] this->PisoPuerta;
+		this->PisoPuerta = new char[strlen(pisoPuerta) + 1];
+		strcpy(this->PisoPuerta, pisoPuerta);
+	}
+
+	// Metodos
+	void Usuario::imprimirUsuario() {
+		// TBD
+	}
+}
---
+++ Cliente/Clases/clases.h
@@ -0,0 +1,171 @@
+#ifndef _CLASES_H_
+#define _CLASES_H_
+
+namespace clases {
+
+    class Cartera {
+    private:
+        int ID_Cartera;
+        float saldo;
+        int ID_Usuario;
+    public:
+        Cartera(int idCartera, float saldo, int idUsuario);
+        Cartera(const Cartera& c);
+
+    };
+
+    class Transaccion {
+    private:
+        int ID_Transaccion;
+        float Cantidad;
+        int Fecha;
+        int ID_Objeto;
+        int ID_Cartera_Recibe;
+        int ID_Cartera_Envia;
+    public:
+        // Constructores y destructores
+        Transaccion(int idTrans, float cantidad, int fecha, int idObjeto, int idCartEnvia, int idCartRec);
+        Transaccion(const Transaccion& t);
+
+        // Getters y Setters
+        int getIDTrans();
+        float getCant();
+        int getFecha();
+        int getIDObjeto();
+        int getIDCarteraRecibe();
+        int getIDCarteraEnvia();
+
+        void setIDTrans(int id);
+        void setCant(int cant);
+        void setFecha(int fecha);
+        void setIDObjeto(int idObjeto);
+        void setIDCarteraRecibe(int idCartera);
+        void setIDCarteraEnvia(int idCartera);
+
+        // Metodos
+    };
+
+    class Objeto {
+    private:
+        int ID_Objeto;
+        char* Categoria;
+        char* Estado;
+        char* Descripcion;
+        float PrecioSalida;
+        int ID_Subastador;
+        int ID_Lote;
+    public:
+        // Constructores y destructores
+        Objeto(int idObjeto, char* cat, char* estado, char* desc, float precioSal, int idSubastador, int idLote);
+        Objeto(const Objeto& o);
+        ~Objeto();
+        // Getters y Setters
+ 
+        int getIDObjeto();
+        char* getCategoria();
+        char* getEstado();
+        char* getDescripcion();
+        float getPrecioSalida();
+        int getIDSubastador();
+        int getID_Lote();
+
+        void setIDObjeto(int id);
+        void setCategoria(char* categoria);
+        void setEstado(char* estado);
+        void setDescripcion(char* descripcion);
+        void setPrecioSalida(float precio);
+        void setIDSubastador(int id);
+        void setIDLote(int id);
+
+        // Metodos
+        void imprimirObjeto();
+    };
+
+    class Lote {
+    private:
+        int idLote;
+        int fechaCom;
+        int fechaFin;
+        char* estado;
+        int avgPrecio; // del 1 al 4 dependiendo del precio medio [$, $$, $$$, $$$$]
+        int cantidadObjetos;
+        Objeto* listaObjetos;
+    public:
+        // Constructores y destructores
+        Lote(int idLote, int fechaCom, int fechaFin, char* estado, int avgPrecio, int cantObj, Objeto* listaObjetos);
+        Lote(const Lote& l);
+        ~Lote();
+        // Getters y Setters
+
+        int getIDLote();
+        int getFechaCom();
+        int getFechaFin();
+        char* getEstado();
+        int getAvgPrecio();
+        int getCantidadObjetos();
+        Objeto* getListaObjetos();
+
+        void setIDLote(int id);
+        void setFechaCom(int fecha);
+        void setFechaFin(int fecha);
+        void setEstado(char* estado);
+        // setAvgPrecio no tiene sentido, se tiene que calcular en el constructor y al añadir/eliminar objetos
+        void setCantidadObjetos(int cant);
+        void setListaObjetos(Objeto* listaObjetos);
+
+        // Metodos
+        void anadirObjeto(Objeto& o);
+        void calcularAvgPrecio();
+        void eliminarObjeto(int idObjeto);
+        void imprimirLote();
+    };
+
+    class Usuario {
+    private:
+        int ID_Usuario;
+        char* Contrasenia;
+        char* Nombre;
+        int Tlf;
+        char* Mail;
+        int Puntos;
+        int ID_Cartera;
+        char* Pais;
+        char* Ciudad;
+        char* Calle;
+        char* PisoPuerta;
+    public:
+        // Constructores y destructores
+        Usuario(int idUsuario, char* contra, char* nombre, int tlf, char* mail, int puntos, int idCartera, char* pais, char* ciudad, char* calle, char* pisoPuerta);
+        Usuario(const Usuario& u);
+        ~Usuario();
+        // Getters y Setters
+
+        int getIDUsuario();
+        char* getContrasenia();
+        char* getNombre();
+        int getTlf();
+        char* getMail();
+        int getPuntos();
+        int getIDCartera();
+        char* getPais();
+        char* getCiudad();
+        char* getCalle();
+        char* getPisoPuerta();
+
+        void setIDUsuario(int id);
+        void setContrasenia(char* contrasenia);
+        void setNombre(char* nombre);
+        void setTlf(int tlf);
+        void setMail(char* mail);
+        void setPuntos(int puntos);
+        void setIDCartera(int id);
+        void setPais(char* pais);
+        void setCiudad(char* ciudad);
+        void setCalle(char* calle);
+        void setPisoPuerta(char* pisoPuerta);
+
+        // Metodos
+        void imprimirUsuario();
+    };
+}
+#endif
