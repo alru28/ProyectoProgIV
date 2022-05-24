b1d9f06be7644072d63d173f0b813822638118f1
jorgealcorta <jorgealcorta@opendeusto.es>
2022-05-24 10:48:58 +0200

    Creacion Formateador

---
+++ Servidor/Formateador/Formateador.h
@@ -0,0 +1,4 @@
+#ifndef FORMATEADOR_H_
+#define FORMATEADOR_H_
+
+//Formateador convierte lo que hay en C en el formato con separadores
--- Servidor/Parser/Parser.h
+++ Servidor/Parser/Parser.h
@@ -1,7 +1,8 @@
 #ifndef PARSER_H_
 #define PARSER_H_
+#include "Cartera.h"
 
-
+//Parser son los que leen el string gordo con separadores y lo interpretan
 
 #endif /* PARSER_H_*/
 
