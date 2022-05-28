3b47e9b8a1d9945d742113ce99c0de86abe1fab0
Unai Igartua <unaiigartua@opendeusto.es>
2022-05-28 23:24:13 +0200

    comando para hacer los .o de servidor :fire:  :fire:

--- Servidor/Main/Main.cpp
+++ Servidor/Main/Main.cpp
@@ -1,5 +1,8 @@
 #include "../Socket/serverSocket.h"
 
+// estando desde carpeta Servidor, comando para hacer todos los .o de servidor:
+// cd BaseDatos && g++ -c GestorBaseDatos.cpp -o GestorBaseDatos.o && cd .. && cd Logger && g++ -c Logger.cpp -o Logger.o && cd .. && cd Socket && g++ -c serverSocket.cpp -o serverSocket.o && cd .. && cd Main && g++ -c main.cpp -o main.o
+
 // g++ -o Main.exe Main.o ../BaseDatos/GestorBaseDatos.o ../Logger/Logger.o ../Socket/serverSocket.o ../BaseDatos/sqlite3.o -lws2_32
 
 int main() {
