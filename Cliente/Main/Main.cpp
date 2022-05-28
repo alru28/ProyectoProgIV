7becd31459655247fb44e2f23cd59f2b7d2c0a5b
nicllop <nicllop@opendeusto.es>
2022-05-28 23:38:42 +0200

    comando unico funcional

--- Cliente/Main/Main.cpp
+++ Cliente/Main/Main.cpp
@@ -2,9 +2,8 @@
 #include "../Menu/menu.h"
 
 // estando desde carpeta cliente, prueba para hacer todos los .o de cliente:
-// cd Soket && g++ -c clienteShoket.cpp -o clienteShoket.o && cd .. && cd Clases && g++ -c clases.cpp -o clases.o && cd .. && cd Menu && g++ -c menu.cpp -o menu.o && cd .. && cd Main && g++ -c main.cpp -o main.o
+// cd Socket && g++ -c clientSocket.cpp -o clientSocket.o && cd .. && cd Clases && g++ -c clases.cpp -o clases.o && cd .. && cd Menu && g++ -c menu.cpp -o menu.o && cd .. && cd Main && g++ -c Main.cpp -o Main.o && g++ -o Main.exe Main.o ../Socket/clientSocket.o ../Clases/clases.o ../Menu/Menu.o -lws2_32
 
-// g++ -o Main.exe Main.o ../Socket/clientSocket.o ../Clases/clases.o ../Menu/Menu.o -lws2_32
 
 int main() {
 	ClientSocket::startSocket();
