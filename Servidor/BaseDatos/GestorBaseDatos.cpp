1b838d4f1828ab9f8e0385f5740b8d0a4e02ef47
jorgealcorta <jorgealcorta@opendeusto.es>
2022-05-28 17:40:14 +0200

    mostrarLotesActivos primer approach :goat:

--- Servidor/BaseDatos/GestorBaseDatos.cpp
+++ Servidor/BaseDatos/GestorBaseDatos.cpp
@@ -260,3 +260,56 @@ int GestorBD::introducirUsuario(char* texto){
     return idUser;
     
 }
+
+char* GestorBD::mostrarLotesActivos(){
+    sqlite3_stmt *stmt;
+    char bruto[1000];
+    
+    char sql[100];
+    sprintf(sql, "select ID_Lote, FechaCom, FechaFin, AvgPrecio from lote where Estado = 'En curso'");
+    
+    int result = sqlite3_prepare_v2(GestorBD::baseDatos, sql, -1, &stmt, NULL) ;
+	if (result != SQLITE_OK) {
+		printf("Error preparing statement (SELECT)\n");
+		printf("%s\n", sqlite3_errmsg(GestorBD::baseDatos));
+	}
+    printf("\nLotes activos actualmente :\n");
+
+	do {
+		result = sqlite3_step(stmt) ;
+		if (result == SQLITE_ROW) {
+			int id= sqlite3_column_int(stmt, 0);
+
+            char fechaInicio[20];
+			strcpy(fechaInicio, (char *) sqlite3_column_text(stmt, 1));
+
+            char fechaFinal[20];
+			strcpy(fechaFinal, (char *) sqlite3_column_text(stmt, 2));
+
+            float avgPrecio = (float) sqlite3_column_double(stmt, 3);
+            char* dolars;
+
+            if(avgPrecio<10.00){
+                dolars = "$";
+            } else if(avgPrecio <30.00){
+                dolars = "$$";
+            }else if(avgPrecio < 60.00){
+                dolars = "$$$";
+            }else{
+                dolars = "$$$$";
+            }
+            char temp[300];
+            sprintf(temp, "%i;%s;%s;%.2f;%s;|", id, fechaInicio, fechaFinal, avgPrecio, dolars); //Concatenamos todos los atributos del lote actual
+            strcat(bruto, temp); //Metemos el lote actual en el tocho
+		}
+	} while (result == SQLITE_ROW);
+
+    strcat(bruto, "#");//Fin del bruto
+
+    result = sqlite3_finalize(stmt);
+	if (result != SQLITE_OK) {
+		printf("Error finalizing statement (SELECT)\n");
+		printf("%s\n", sqlite3_errmsg(GestorBD::baseDatos));
+	}
+    
+}
