ab2d569cd43f089f61e9f6803f177f37c5fd8a8a
jorgealcorta <jorgealcorta@opendeusto.es>
2022-05-30 19:16:54 +0200

    parser funciona :)

--- Cliente/Menu/testParserManual.cpp
+++ Cliente/Menu/testParserManual.cpp
@@ -17,6 +17,7 @@ int main(){
 
     //Get first token de barras
     char* tokenGrande = strtok(bigString, "|");
+    char atributo[20];
     while(tokenGrande != NULL)
     {
         //cout << tokenGrande[1] << " " << tokenGrande[2] << " " << tokenGrande[3] << " " << tokenGrande[4] << endl;
@@ -24,19 +25,36 @@ int main(){
         i=0;
         for(int j=0; j<4; j++)
         {
+                        
+            switch (j)
+            {
+            case 0:
+                strcpy(atributo, "ID: ");
+                break;
+            case 1:
+                strcpy(atributo, "- Fecha de inicio: ");
+                break;
+            case 2:
+                strcpy(atributo, "- Fecha de finalizacion: ");
+                break;
+            case 3:
+                strcpy(atributo, "- Precio: ");
+                break;
+            default:
+                break;
+            }
+            cout << " " << atributo;
+            //PRINTEAR CHARS HASTA LLEGAR A ;
             while(tokenGrande[i] != ';')
             {
                 cout << tokenGrande[i];
                 i++;
             }
-            cout<<endl;
             i++;
         }
-        cout<<endl<<endl<<endl;
-        
+        cout<<endl;
         //Siguiente token
         tokenGrande = strtok(NULL, "|");
-        cout << tokenGrande;
     }
     
 }
